4d6cedd9492beda2929acf05a673dd1a3f41a4eb
miszx <miszx@misz>
2017-04-12 22:03:22 +0200

    Testy i poprawiony algorytm BestSequence.

--- zajecia04-algorytmy/src/Zadanie1BestSequence.java
+++ zajecia04-algorytmy/src/Zadanie1BestSequence.java
@@ -19,8 +19,10 @@ public class Zadanie1BestSequence {
 		int maxEnd = 0;
 
 		for (int i = 0; i < array.length - 1; i++) {
+			System.out.println(array[i] + ">" + array[i + 1]);
 			if (array[i] > array[i + 1]) {
 				actualEnd = i;
+				System.out.println("beg " + actualBeginning + " end " + actualEnd);
 				if ((actualEnd - actualBeginning) >= (maxEnd - maxBeginning)) {
 					maxBeginning = actualBeginning;
 					maxEnd = actualEnd;
@@ -30,7 +32,14 @@ public class Zadanie1BestSequence {
 				}
 			}
 		}
-
+		
+		// Warunek w pętli powyżej nie wykona się dla ostatniego ciągu ponieważ pętla dojdzie do końca tablicy!
+		// Należy sprawdzić czy ostatni ciąg w tablicy jest dłuższy od tego który wyszedł najdłuższy z pętli.
+		if ((array.length - actualEnd + 1) > (maxEnd - maxBeginning + 1)) {
+			maxBeginning = actualEnd + 1;
+			maxEnd = array.length - 1;
+		}
+		
 		int indx = 0;
 		int[] bestSequence = new int[maxEnd + 1 - maxBeginning];
 		for (int j = maxBeginning; j <= maxEnd; j++) {
@@ -41,7 +50,7 @@ public class Zadanie1BestSequence {
 	}
 
 	public static void main(String[] args) {
-		int[] test = { 1, 2, 300, 1, 2, 3, 4, 1, 2 };
+		int[] test = { 1, 2, 300, 1, 2, 3, 4, 1, 2, 0, 4, 5, 1, 7, 8, 1, 2, 3};
 		int[] wynik = bestSequence(test);
 		 for (int i : wynik) {
 		 System.out.print(i + " ");
