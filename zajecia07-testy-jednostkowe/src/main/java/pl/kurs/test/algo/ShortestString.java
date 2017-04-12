868e7086746a782b5305c496e84c0150b6b56886
miszx <miszx@misz>
2017-04-12 22:02:34 +0200

    Testy i poprawiony algorytm BestSequence.

--- zajecia07-testy-jednostkowe/src/main/java/pl/kurs/test/algo/ShortestString.java
+++ zajecia07-testy-jednostkowe/src/main/java/pl/kurs/test/algo/ShortestString.java
@@ -1,5 +1,6 @@
 package pl.kurs.test.algo;
 //s1 = "abc" s2="def"
+
 // c = abcdef //zero czesci wspolnych i ani jedno ani drugie nie zawiera sie w sobie
 
 // s1 = "abc" s2="bcd"
@@ -14,17 +15,45 @@ package pl.kurs.test.algo;
 public class ShortestString {
 	public String findShortest(String s1, String s2) {
 
-		if (!s1.contains(s2) || !s2.contains(s1)) {
-			return s1 + s2;
-		} 
-		
-		char[] s1Chars = s1.toCharArray();
-		for (int i = 1; i <s1.length(); i++) {
+		// if (!s1.contains(s2) || !s2.contains(s1)) {
+		// return s1 + s2;
+		// }
+
+		for (int i = 1; i < s1.length(); i++) {
 			String beg = s1.substring(0, i);
-			//contains, startsWith, endsWith
+			if (s2.startsWith(beg)) {
+				return s1 + s2.substring(i + 1, s2.length());
+			}
 		}
-	
+		
+		for (int i = 1; i < s1.length(); i++) {
+			String beg = s1.substring(i, s1.length());
+			if (s2.startsWith(beg)) {
+				return s1 + s2.substring(i + 1, s2.length());
+			}
+		}
+		
+		if (s1.contains(s2)) {
+			return s1;
+		} else if (s2.contains(s1)) {
+			return s2;
+		}
+
+		for (int i = 1; i < s1.length(); i++) {
+			String beg = s1.substring(0, i);
+			if (s2.endsWith(beg)) {
+				return s2 + s1.substring(i, s1.length());
+			}
+		}
 		
 		return null;
+	}
+
+	public static void main(String[] args) {
+		ShortestString test = new ShortestString();
+		System.out.println(test.findShortest("abxxsc123", "dabxxs"));
+//		System.out.println(test.findShortest("c", "abc"));
+//		System.out.println(test.findShortest("abc", "bce"));
+//		System.out.println(test.findShortest("abcdef", "abhfe"));
 	}
 }
